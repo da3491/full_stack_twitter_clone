3f61aae35906208a71d21080148c4e27733d4826
da3491 <d.annese3491@gmail.com>
2022-11-17 14:29:55 -0500

    Start of Homepage Stylings

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -19,38 +19,45 @@ const Home = () => (
         </div>
       </div>
     </nav>
-    <main>
-      <div>
-        <h1>Welcome to Twitter.</h1>
-        <p>Connect with your friends - and other fascinating people. Get in-the-moment updates on the things that interest you.  And watch events unfold, in real time, from every angle.</p>
+    <main className='container row m-auto mt-5'>
+      <div className='col-7 d-flex flex-column justify-content-between'>
+        <div className='me-lg-5'>
+          <h1 className='mb-4'>Welcome to Twitter.</h1>
+          <p>Connect with your friends - and other fascinating people. Get in-the-moment updates on the things that interest you.  And watch events unfold, in real time, from every angle.</p>
+        </div>
         <div>
           <div>Hack Pacific - Backendium Twitter Project</div>
           <a>Tweet and photo by @Hackpacific 3:20PM - 15 December 2016</a>
         </div>
       </div>
-      <div>
-        <div id="section__id">
-          <input placeholder='Username'></input>
-          <div>
-            <input placeholder='Password'></input>
-            <button>Log in</button>
-          </div>
-          <div>
-            <div>checkbox</div>
-            <a href="#">Forgot password?</a>
+      <div className='col-5'>
+        <div id="section__id" className='border rounded mb-3'>
+          <div className='d-flex flex-column gap-3 m-3'>
+            <input className='w-100' placeholder='Username'></input>
+            <div className='d-flex justify-content-between'>
+              <input className='flex-shrink-1 ' placeholder='Password'></input>
+              <button className='btn btn-primary btn-sm text-nowrap'>Log in</button>
+            </div>
+            <div className='d-flex'>
+              <div className='d-flex'>
+                <input type='checkbox' className='me-2' />
+                <div className='me-2'>Remember Me</div>
+              </div>
+              <a href="#" className='text-decoration-none'>Forgot password?</a>
+            </div>
           </div>
         </div>
-        <div id='section__signup'>
+        <div id='section__signup' className='border rounded p-3'>
           <div>
-            <strong>New to Twitter?</strong>
-            <a href="#">Sign up</a>
+            <strong className='pe-2'>New to Twitter?</strong>
+            <a href="#" className='text-decoration-none text-secondary'>Sign up</a>
           </div>
-          <div>
-            <input placeholder='Username'></input>
-            <input placeholder="Email"></input>
-            <input placeholder='Password'></input>
+          <div className='input-group'>
+            <input className='my-2 w-100 form-control' placeholder='Username'></input>
+            <input className='my-2 w-100 form-control' placeholder="Email"></input>
+            <input className='my-2 w-100 form-control' placeholder='Password'></input>
           </div>
-          <button>Sign up for Twitter</button>
+          <button className='btn btn-warning fw-bold'>Sign up for Twitter</button>
         </div>
         <div></div>
       </div>
